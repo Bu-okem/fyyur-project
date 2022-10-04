8ce9382337f78575f3319297edcee438d03ca974
Bu-okem <Chiobiegbulem@gmail.com>
2022-10-04 16:08:34 +0100

    configured app for heroku

--- app/config.py
+++ app/config.py
@@ -10,4 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://buokem:buokem@localhost:5432/fyyur'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://buokem:buokem@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://gnlssslhwufyzz:8d9102e1760799256235f693a971ea5b833764878fb9fd898dd7d615ae7a1ffc@ec2-3-92-98-129.compute-1.amazonaws.com:5432/d3kgcih8d3tlee'
