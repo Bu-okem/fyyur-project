2088c7f194345fbb98a6d588bee31081b582737d
Bu-okem <Chiobiegbulem@gmail.com>
2022-10-07 06:30:52 +0100

    Fix show creating error

--- app/app.py
+++ app/app.py
@@ -22,13 +22,13 @@ import os
 # App Config.
 #----------------------------------------------------------------------------#
 
-SECRET_KEY = os.urandom(32)
 app = app
+app.config['SECRET_KEY'] = os.urandom(32)
 moment = Moment(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 #app.config.from_object('config')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://apajblqiprtuxn:bcff78238e24e158b26aa866f32d549c1295a96d23b2c2ab594fd42b18c86270@ec2-3-92-98-129.compute-1.amazonaws.com:5432/df6ein33quthou'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://buokem:buokem@localhost:5432/fyyur'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://apajblqiprtuxn:bcff78238e24e158b26aa866f32d549c1295a96d23b2c2ab594fd42b18c86270@ec2-3-92-98-129.compute-1.amazonaws.com:5432/df6ein33quthou'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://buokem:buokem@localhost:5432/fyyur'
 migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
