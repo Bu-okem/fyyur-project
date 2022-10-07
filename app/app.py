6e6771c2916fbda4c2a47c55b6c79a7fa76a27e2
Bu-okem <Chiobiegbulem@gmail.com>
2022-10-07 07:08:45 +0100

    Fix error

--- app/app.py
+++ app/app.py
@@ -23,10 +23,10 @@ import os
 #----------------------------------------------------------------------------#
 
 app = app
-app.config['SECRET_KEY'] = os.urandom(32)
 moment = Moment(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 #app.config.from_object('config')
+app.config['SECRET_KEY'] = os.urandom(32)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://apajblqiprtuxn:bcff78238e24e158b26aa866f32d549c1295a96d23b2c2ab594fd42b18c86270@ec2-3-92-98-129.compute-1.amazonaws.com:5432/df6ein33quthou'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://buokem:buokem@localhost:5432/fyyur'
 migrate = Migrate(app, db)
